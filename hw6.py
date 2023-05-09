a76cd7c7ea7875ddddeeb4910d5c10f36aa41954
kim hoehwan <74888623+hoihwan@users.noreply.github.com>
2023-05-09 12:08:11 +0900

    Create hw6.py

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for j in range(1, 10, 1):
+    for i in range(2, 6, 1):
+        print(i, "x", j, "=", i*j, "\t", end = "")
+    print(" ")
+
+print(" ")
+
+for j in range(1, 10, 1):
+    for i in range(6, 10, 1):
+        print(i, "x", j, "=", i*j, "\t", end = "")
+    print(" ")
